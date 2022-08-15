985bf24bed72f9c56bb7fbc3b77deb99a659f30e
linus mutugi mate <mlynux@gmail.com>
2022-08-15 10:53:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+
+
+const companyName = 'Scuber';
+companyName;
+
+let mostProfitableNeighborhood = "Chelsea";
+mostProfitableNeighborhood;
+
+let companyCeo = "Susan Smith";
+companyCeo;
